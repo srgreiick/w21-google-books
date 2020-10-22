f5a1f6e2dcaf32c671c0c527e30359df967678eb
srgreiick <samgreilick@gmail.com>
2020-10-22 17:04:29 -0600

    api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export default {
+  //Get books from google
+  getBooksGoogle: function(search) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" +search)},
+
   // Gets all books
   getBooks: function() {
     return axios.get("/api/books");
