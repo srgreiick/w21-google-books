5250593d34466806cb88b25ed92239925fb50a18
srgreiick <samgreilick@gmail.com>
2020-10-24 16:27:12 -0600

    cleaned up models

--- models/book.js
+++ models/book.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
+  authors: [{type: String, required: true}],
+  description: String,
+  image: {type: String, trim: true},
+  link: {type: String, trim: true},
   date: { type: Date, default: Date.now }
 });
 
