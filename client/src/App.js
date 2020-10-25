0115a2faa94a0a2ef011bd3ac85e8fda769d7dce
srgreiick <samgreilick@gmail.com>
2020-10-25 13:43:52 -0600

    added pages info and updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SavedBooks from "./pages/SavedBooks";
 import BookSearch from "./pages/BookSearch";
-import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import Footer from "./components/Footer"
-import "./App.css"
+
 
 
 function App() {
@@ -17,9 +15,7 @@ function App() {
           <Route exact path="/" component={BookSearch} />
           <Route exact path="/saved" component={SavedBooks} />
           <Route exact path="/saved/:id" component={SavedBooks} />
-          <Route component={NoMatch} /> 
         </Switch>
-        <Footer />
       </div>
     </Router>
   );
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import API from "../utils/API";
 import Jumbotron from "../components/Jumbotron";
 import { Container} from "../components/Grid";
-import SavedResult from "../components/SavedResult"
+import SavedResult from "../components/SavedBooks"
 
 class SaveBook extends Component {
     state = {
